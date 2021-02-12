7f92e314fd93c9af0ad82bc4206efc1e2f41a4e8
Geoffrey Harrison <geoffh1977@gmail.com>
2021-02-12 14:58:06 +1100

    Update CircleCI With Better If Statement

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -7,7 +7,7 @@ LATEST_TAG=$(curl -s https://hub.docker.com/v2/repositories/geoffh1977/minecraft
 DOWNLOAD_FILE=$(curl -s https://www.minecraft.net/en-us/download/server/bedrock/ | grep -o "https://.*bin-linux/bedrock-server-.*.zip" | head -1)
 VERSION=$(echo "${DOWNLOAD_FILE}" |  grep -Eo "[0-9]{1,3}\.[0-9]{1,3}(\.[0-9]{1,3}\.[0-9]{1,3})?")
 
-# Output
+# Display Results
 echo
 echo "Latest Stored Docker Build Tag: ${LATEST_TAG}"
 echo "Current Live Software Version: ${VERSION}"
@@ -16,11 +16,9 @@ echo
 if [ "${LATEST_TAG}" == "${VERSION}" ]
 then
   echo "Versions Match. No Rebuild Is Required."
-  EXIT_CODE=1
+  echo "export REBUILD_REQUIRED=false" >> "${BASH_ENV}"
 else
   echo "Version MisMatch. A Rebuild Is Required."
-  EXIT_CODE=0
+  echo "export REBUILD_REQUIRED=true" >> "${BASH_ENV}"
 fi
-
 echo
-exit ${EXIT_CODE}
