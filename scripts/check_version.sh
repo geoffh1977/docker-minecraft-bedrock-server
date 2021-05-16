7018da994536681f750e3a468136b42498f178fd
Geoffrey Harrison <geoffh1977@gmail.com>
2021-05-16 18:20:19 +1000

    Fix Check Version Code

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -4,7 +4,7 @@
 LATEST_TAG=$(curl -s https://hub.docker.com/v2/repositories/geoffh1977/minecraft-bedrock-server/tags | jq -r '.results[].name' | grep -v "[a-z]" | sort -rn | head -n1)
 
 # Get Live Software Version
-DOWNLOAD_FILE=$(curl -s https://www.minecraft.net/en-us/download/server/bedrock/ | grep -o "https://.*bin-linux/bedrock-server-.*.zip" | head -1)
+DOWNLOAD_FILE=$(curl -Ls https://www.minecraft.net/en-us/download/server/bedrock | grep -o "https://.*bin-linux/bedrock-server-.*.zip" | head -1)
 VERSION=$(echo "${DOWNLOAD_FILE}" |  grep -Eo "[0-9]{1,3}\.[0-9]{1,3}(\.[0-9]{1,3}\.[0-9]{1,3})?")
 
 # Display Results
